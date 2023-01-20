5674e45eef835350c27bc0fdd3e091e1ec4fe083
Bolmgren1 <kebo05001@utb.vaxjo.se>
2023-01-20 10:22:53 +0100

    Add project files.

---
+++ Uppgift 5.2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många heltal vill du skriva in?");
+            string mängd = Console.ReadLine();
+
+            int mängden = int.Parse(mängd);
+;
+            int[] m = new int[mängden];
+
+            for(int i =0; i < mängden; i++)
+            {
+
+                Console.Write("Skriv in ditt tal: ");
+                m[i]=int.Parse(Console.ReadLine());
+
+            }
+
+            foreach(int i in m)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
+
